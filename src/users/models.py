4f224e3da7ff0a86ce32da076613aea08fb8325b
Nurdaulet <dicenameismy@gmail.com>
2022-07-17 22:26:27 +0600

    Use uuid as user primary key

--- src/users/models.py
+++ src/users/models.py
@@ -1,7 +1,12 @@
+import uuid
+from django.db import models
+
 from django.contrib.auth.models import AbstractUser, UserManager, AnonymousUser as DjangoAnonymousUser
 
 
 class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
     objects: UserManager["User"] = UserManager()  # type: ignore[assignment]
 
 
